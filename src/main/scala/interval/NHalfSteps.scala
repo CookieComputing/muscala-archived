a972e339c911bdd962944dfbba207bfb36e744d1
Kevin Hui <kevin.hui@joinhoney.com>
2020-02-23 17:39:32 -0800

    add NHalfStep class

---
+++ src/main/scala/interval/NHalfSteps.scala
@@ -0,0 +1,9 @@
+package interval
+
+import note.Note
+
+/**
+ * Provides movements from an arbitrary N steps. You probably want to use the Whole Step or Half Step classes for the
+ * basic movements.
+ */
+case class NHalfSteps(note: Note, interval: Int) extends IntervalMovement
