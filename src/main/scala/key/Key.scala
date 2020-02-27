c25bdee2230f8028a42202c87c1460d8f1f436cb
Kevin Hui <kevin.hui@joinhoney.com>
2020-02-26 21:57:33 -0800

    run scalafmt

--- src/main/scala/key/Key.scala
+++ src/main/scala/key/Key.scala
@@ -4,41 +4,49 @@ import note.Note
 
 /**
   * Represents a key.
- *
- * @param tonic the tonic of the key
+  *
+  * @param tonic the tonic of the key
   */
-case class Key private (tonic: String, signature: List[String]) {
-}
+case class Key private (tonic: String, signature: List[String]) {}
 
 object Key {
   private val KeyRegex = "^[A-G](?:#*|b*)$".r
   private val baseKey = "C"
   private val baseKeySignature = List("C", "D", "E", "F", "G", "A", "B")
-  private val ordering = Map("F" -> 0, "C" -> 1, "G" -> 2, "D" -> 3, "A" -> 4, "E" -> 5, "B" -> 6)
+  private val ordering =
+    Map("F" -> 0, "C" -> 1, "G" -> 2, "D" -> 3, "A" -> 4, "E" -> 5, "B" -> 6)
 
   /**
-   * @param tonic the tonic to create a key from
-   * @return a key if the tonic is valid or none otherwise
-   */
+    * @param tonic the tonic to create a key from
+    * @return a key if the tonic is valid or none otherwise
+    */
   def apply(tonic: String): Option[Key] = tonic match {
-    case KeyRegex() => Some(new Key(tonic, buildKeySignature(baseKey, tonic, baseKeySignature)))
+    case KeyRegex() =>
+      Some(new Key(tonic, buildKeySignature(baseKey, tonic, baseKeySignature)))
     case _ => None
   }
 
   // Follows the following heuristics for converting to another key signature
   // Sharping: Take the basic key signature, use the fifth as the new root, sharp the old 4th
   // Flatting: Take the basic key signature, use the fourth as the new root, sharp the old 7th
-  private def buildKeySignature(tonic: String, endTonic: String, signature: List[String]): List[String] = {
+  private def buildKeySignature(
+      tonic: String,
+      endTonic: String,
+      signature: List[String]
+  ): List[String] = {
     val buildNewTonic = (buildNewSignature: (List[String] => List[String])) => {
       val newList = buildNewSignature(signature)
       buildKeySignature(newList.head, endTonic, newList)
     }
     endTonic match {
-      case _ if tonic == endTonic => signature // we have matched the signature, we are done
+      case _ if tonic == endTonic =>
+        signature // we have matched the signature, we are done
       case _ if endTonic.last == Note.Sharp => buildNewTonic(sharpKeySignature)
-      case _ if endTonic.last == Note.Flat => buildNewTonic(flatKeySignature)
-      case _ if ordering(tonic) < ordering(endTonic) => buildNewTonic(sharpKeySignature)
-      case _ if ordering(tonic) > ordering(endTonic) => buildNewTonic(flatKeySignature)
+      case _ if endTonic.last == Note.Flat  => buildNewTonic(flatKeySignature)
+      case _ if ordering(tonic) < ordering(endTonic) =>
+        buildNewTonic(sharpKeySignature)
+      case _ if ordering(tonic) > ordering(endTonic) =>
+        buildNewTonic(flatKeySignature)
     }
   }
 
@@ -57,10 +65,12 @@ object Key {
   // D E F# G A B C#
   private def sharpKeySignature(signature: List[String]): List[String] = {
     val sharpedFourth = sharpNote(signature(3))
-    signature.slice(4,7) ++ signature.take(3) ++ List(sharpedFourth)
+    signature.slice(4, 7) ++ signature.take(3) ++ List(sharpedFourth)
   }
 
-  private def sharpNote(note: String) = if (note.last == Note.Flat) note.drop(1) else note + Note.Sharp
+  private def sharpNote(note: String) =
+    if (note.last == Note.Flat) note.drop(1) else note + Note.Sharp
 
-  private def flatNote(note: String) = if (note.last == Note.Sharp) note.drop(1) else note + Note.Flat
+  private def flatNote(note: String) =
+    if (note.last == Note.Sharp) note.drop(1) else note + Note.Flat
 }
--- src/test/scala/key/KeyTest.scala
+++ src/test/scala/key/KeyTest.scala
@@ -3,24 +3,58 @@ package key
 import org.scalatest.FunSuite
 
 /**
- * Unit tests for the Key class
- */
+  * Unit tests for the Key class
+  */
 class KeyTest extends FunSuite {
   test("Natural letter keys are valid") {
     List("A", "B", "C", "D", "E", "F", "G")
       .map { Key(_) }
-      .map {key => assert(key.isDefined) }
+      .map { key =>
+        assert(key.isDefined)
+      }
   }
 
   test("Keys with only sharps or flats are valid") {
-    List("A#", "Bb", "B#", "Cb", "C#","Db", "D#", "Eb", "E#", "Fb", "F#", "Gb", "G#", "Ab")
-      .map { Key(_) }
-      .map {key => assert(key.isDefined) }
+    List(
+      "A#",
+      "Bb",
+      "B#",
+      "Cb",
+      "C#",
+      "Db",
+      "D#",
+      "Eb",
+      "E#",
+      "Fb",
+      "F#",
+      "Gb",
+      "G#",
+      "Ab"
+    ).map { Key(_) }
+      .map { key =>
+        assert(key.isDefined)
+      }
 
     // Double accidentals and other possible theoretical keys are acceptable!
-    List("A##", "Bbb", "B##", "Cbb", "C##","Dbb", "D##", "Ebb", "E##", "Fbb", "F##", "Gbb", "G##", "Abb")
-      .map { Key(_) }
-      .map {key => assert(key.isDefined) }
+    List(
+      "A##",
+      "Bbb",
+      "B##",
+      "Cbb",
+      "C##",
+      "Dbb",
+      "D##",
+      "Ebb",
+      "E##",
+      "Fbb",
+      "F##",
+      "Gbb",
+      "G##",
+      "Abb"
+    ).map { Key(_) }
+      .map { key =>
+        assert(key.isDefined)
+      }
   }
 
   test("Natural letter keys return expected key signatures") {
@@ -38,22 +72,22 @@ class KeyTest extends FunSuite {
   }
 
   test("The entire circle of fifths is supported") {
-   List (
-     ("Cb", List("Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb")),
-     ("Gb", List("Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F")),
-     ("Db", List("Db", "Eb", "F", "Gb", "Ab", "Bb", "C")),
-     ("Ab", List("Ab", "Bb", "C", "Db", "Eb", "F", "G")),
-     ("Eb", List("Eb", "F", "G", "Ab", "Bb", "C", "D")),
-     ("Bb", List("Bb", "C", "D", "Eb", "F", "G", "A")),
-     ("F", List("F", "G", "A", "Bb", "C", "D", "E")),
-     ("C", List("C", "D", "E", "F", "G", "A", "B")),
-     ("G", List("G", "A", "B", "C", "D", "E", "F#")),
-     ("D", List("D", "E", "F#", "G", "A", "B", "C#")),
-     ("A", List("A", "B", "C#", "D", "E", "F#", "G#")),
-     ("E", List("E", "F#", "G#", "A", "B", "C#", "D#")),
-     ("B", List("B", "C#", "D#", "E", "F#", "G#", "A#")),
-     ("F#", List("F#", "G#", "A#", "B", "C#", "D#", "E#")),
-     ("C#", List("C#", "D#", "E#", "F#", "G#", "A#", "B#")),
+    List(
+      ("Cb", List("Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb")),
+      ("Gb", List("Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F")),
+      ("Db", List("Db", "Eb", "F", "Gb", "Ab", "Bb", "C")),
+      ("Ab", List("Ab", "Bb", "C", "Db", "Eb", "F", "G")),
+      ("Eb", List("Eb", "F", "G", "Ab", "Bb", "C", "D")),
+      ("Bb", List("Bb", "C", "D", "Eb", "F", "G", "A")),
+      ("F", List("F", "G", "A", "Bb", "C", "D", "E")),
+      ("C", List("C", "D", "E", "F", "G", "A", "B")),
+      ("G", List("G", "A", "B", "C", "D", "E", "F#")),
+      ("D", List("D", "E", "F#", "G", "A", "B", "C#")),
+      ("A", List("A", "B", "C#", "D", "E", "F#", "G#")),
+      ("E", List("E", "F#", "G#", "A", "B", "C#", "D#")),
+      ("B", List("B", "C#", "D#", "E", "F#", "G#", "A#")),
+      ("F#", List("F#", "G#", "A#", "B", "C#", "D#", "E#")),
+      ("C#", List("C#", "D#", "E#", "F#", "G#", "A#", "B#"))
     ).map {
       case (tonic, expectedSignature) =>
         assert(Key(tonic).get.signature == expectedSignature)
@@ -61,21 +95,38 @@ class KeyTest extends FunSuite {
   }
 
   test("Basic (single accidental) theoretical key signatures are supported") {
-    List (
+    List(
       ("G#", List("G#", "A#", "B#", "C#", "D#", "E#", "F##")),
       ("D#", List("D#", "E#", "F##", "G#", "A#", "B#", "C##")),
       ("A#", List("A#", "B#", "C##", "D#", "E#", "F##", "G##")),
       ("E#", List("E#", "F#", "Eb", "Fb", "Gb", "Ab", "Bb")),
       ("B#", List("B#", "C##", "D##", "E#", "F##", "G##", "A##")),
-      ("Fb", List("Fb", "Gb", "Ab", "Bbb", "Cb", "Db", "Eb")),
+      ("Fb", List("Fb", "Gb", "Ab", "Bbb", "Cb", "Db", "Eb"))
     )
   }
 
   test("Other variations on a key tonic are invalid") {
-    List("Ab#", "Bb#", "Bb#", "Cb#", "Cb#","Db##b#",
-      "D##bb#", "Ebbbbb#", "E######b#", "Fbbbbb#",
-      "F##b#", "Gb##b", "G##bb", "Abbb#", "", "a", "Az")
-      .map { Key(_) }
-      .map {key => assert(key.isEmpty) }
+    List(
+      "Ab#",
+      "Bb#",
+      "Bb#",
+      "Cb#",
+      "Cb#",
+      "Db##b#",
+      "D##bb#",
+      "Ebbbbb#",
+      "E######b#",
+      "Fbbbbb#",
+      "F##b#",
+      "Gb##b",
+      "G##bb",
+      "Abbb#",
+      "",
+      "a",
+      "Az"
+    ).map { Key(_) }
+      .map { key =>
+        assert(key.isEmpty)
+      }
   }
 }
