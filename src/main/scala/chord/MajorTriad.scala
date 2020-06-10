59970de4ede2821b906d3e8890f9cff5388a79c8
Kevin Hui <hui.k@husky.neu.edu>
2020-06-09 23:14:54 -0400

    run scalafmt

--- src/main/scala/chord/MajorTriad.scala
+++ src/main/scala/chord/MajorTriad.scala
@@ -5,9 +5,11 @@ import key.{Key, MajorKey}
 /**
   * Represents a major triad. A major chord is defined as a major third, followed by a minor third.
   */
-case class MajorTriad(override val tonic: String) extends ATriad(tonic, MajorKey(_).get, n => n.major.third)
+case class MajorTriad(override val tonic: String)
+    extends ATriad(tonic, MajorKey(_).get, n => n.major.third)
 
 object MajorTriad {
-  def apply(tonic: String): Option[MajorTriad] = MajorKey(tonic)
-    .map(_ => new MajorTriad(tonic))
+  def apply(tonic: String): Option[MajorTriad] =
+    MajorKey(tonic)
+      .map(_ => new MajorTriad(tonic))
 }
