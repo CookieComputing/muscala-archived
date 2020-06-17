c5244d0e5ed8c15fb8060393e2fb2ee0f5df5cb0
Kevin Hui <hui.k@husky.neu.edu>
2020-06-16 22:10:51 -0400

    add augmented and diminished triad + unit tests

---
+++ src/main/scala/chord/DiminishedTriad.scala
@@ -0,0 +1,21 @@
+package chord
+
+import key.MinorKey
+import note.Note
+
+/**
+  * Represents a diminished triad. A diminished chord is defined as a minor third, followed by a minor third.
+  */
+case class DiminishedTriad private(override val tonic: String)
+  extends ATriad(tonic,
+    MinorKey(_).get,
+    (n, k) => n.minor.third.copy(note = k.notes(2)),
+    (n, k) => n.perfect.fifth.flat.copy(note = Note(k.notes(4)).get.flat.note)) {
+    override def toString: String = tonic + Chord.diminished
+}
+
+object DiminishedTriad {
+    def apply(tonic: String): Option[DiminishedTriad] =
+        MinorKey(tonic)
+          .map(_ => new DiminishedTriad(tonic))
+}
