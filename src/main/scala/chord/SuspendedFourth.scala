6759228f096ac8d16de0a71db97a75d57fc60164
Kevin Hui <hui.k@husky.neu.edu>
2020-06-20 10:38:15 -0400

    ran scalafmt

--- src/main/scala/chord/SuspendedFourth.scala
+++ src/main/scala/chord/SuspendedFourth.scala
@@ -6,11 +6,12 @@ import key.MajorKey
   * A suspended fourth is built from a root, a perfect fourth, and a perfect fifth.
   */
 case class SuspendedFourth private (tonic: String)
-  extends ATriad(tonic,
-    MajorKey(_).get,
-    (n, k) => n.perfect.fourth.copy(note = k.notes(3)),
-    (n, k) => n.perfect.fifth.copy(note = k.notes(4))
-  ) {
+    extends ATriad(
+      tonic,
+      MajorKey(_).get,
+      (n, k) => n.perfect.fourth.copy(note = k.notes(3)),
+      (n, k) => n.perfect.fifth.copy(note = k.notes(4))
+    ) {
   override def toString: String = tonic + Chord.suspended + Chord.fourth
 }
 
--- src/main/scala/chord/SuspendedSecond.scala
+++ src/main/scala/chord/SuspendedSecond.scala
@@ -6,11 +6,12 @@ import key.MajorKey
   * A suspended fourth is built from a root, a major second, and a perfect fifth.
   */
 case class SuspendedSecond private (tonic: String)
-  extends ATriad(tonic,
-    MajorKey(_).get,
-    (n, k) => n.major.second.copy(note = k.notes(1)),
-    (n, k) => n.perfect.fifth.copy(note = k.notes(4))
-  ) {
+    extends ATriad(
+      tonic,
+      MajorKey(_).get,
+      (n, k) => n.major.second.copy(note = k.notes(1)),
+      (n, k) => n.perfect.fifth.copy(note = k.notes(4))
+    ) {
   override def toString: String = tonic + Chord.suspended + Chord.second
 }
 
