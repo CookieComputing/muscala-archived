d3fddde5af00df10e13f232f360ef73914fbb2d9
Kevin Hui <hui.k@husky.neu.edu>
2020-06-25 00:26:51 -0400

    add note generators

--- src/test/scala/helpers/PropertyTesting.scala
+++ src/test/scala/helpers/PropertyTesting.scala
@@ -3,11 +3,25 @@ package helpers
 import chord.Chord
 import note.Note
 import org.scalacheck.Gen
+import org.scalacheck.Arbitrary.arbitrary
 
 /**
   * Helper generators and properties for testing classes in the repository.
   */
 object PropertyTesting {
+
+  // Generates a random natural note of octave 4
+  def naturalNoteGen: Gen[Note] = for {
+    base <- Gen.oneOf(NoteTesting.naturalNoteSeq)
+  } yield base
+
+  // Generates a random note of random octave, with possible sharps or flats.
+  def noteGen: Gen[Note] = for {
+    base <- Gen.oneOf(NoteTesting.naturalNoteSeq.map(_.note))
+    octave <- arbitrary[Int]
+    numOfAccidentals <- Gen.choose(0, 20)
+    accidentals <- Gen.pick(numOfAccidentals, List(Note.Sharp, Note.Flat))
+  } yield Note(base + accidentals.foldLeft("")((acc, char) => acc + char), octave).get
 
   // Given a chord generating function, creates a generator that will generate arbitrary
   // chords to be used in property testing
